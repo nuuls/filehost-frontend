5d1b481b50fd12162666e88dd7d2c450ae850052
nuuls <nilsv21@gmail.com>
2023-01-15 21:10:47 +0100

    fix buttons on mobile

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,7 +4,7 @@
       <router-link to="/" class="logo mx-4">
         <img src="/nuuls-logo.png" class="logo">
       </router-link>
-      <div class="d-flex justify-space-between">
+      <div class="d-flex justify-space-between flex-wrap">
         <router-link v-if="!account" class="invis-link" to="/signup">
           <v-btn>
             Sign up
@@ -15,14 +15,18 @@
             Login
           </v-btn>
         </router-link>
-        <router-link v-if="account" to="/settings" class="invis-link mx-4">
+        <router-link v-if="account" to="/settings" class="invis-link mx-2">
             <v-btn class="justify-self-right">
-              Settings
+              <v-icon icon="mdi-wrench">
+              </v-icon>
+              <p class="mx-2 hidden-sm-and-down">Settings</p>
             </v-btn>
         </router-link>
-        <router-link v-if="account" to="/profile" class="invis-link mx-4">
+        <router-link v-if="account" to="/profile" class="invis-link mx-2">
             <v-btn class="justify-self-right">
-              My Uploads
+              <v-icon icon="mdi-account-circle">
+              </v-icon>
+              <p class="mx-2 hidden-sm-and-down">My Uploads</p>
             </v-btn>
         </router-link>
       </div>
