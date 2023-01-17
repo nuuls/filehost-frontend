8fff293150b3139dbabdbcd3133e59bd7643c565
nuuls <nilsv21@gmail.com>
2023-01-17 20:33:55 +0100

    make auth form submit on enter

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,14 +1,15 @@
 <template>
   <v-container class="d-flex flex-row justify-center align-center h-100">
-    <v-form class="auth-form" ref="form" v-model="valid" lazy-validation>
+    <v-form class="auth-form" ref="form" :submit="submit" v-model="valid" lazy-validation>
       <v-text-field v-model="username" type="username" label="Username" required></v-text-field>
       <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
       <v-text-field v-if="mode === 'signup'" v-model="passwordConfirm" type="password" label="Confirm Password" required></v-text-field>
+      <input type="submit" hidden>
       <v-container class="d-flex w-100 justify-space-around">
-        <v-btn v-if="mode === 'login'" @click="login()">
+        <v-btn v-if="mode === 'login'" @click="submit()">
           Login
         </v-btn>
-        <v-btn v-if="mode === 'signup'" @click="signup()">
+        <v-btn v-if="mode === 'signup'" @click="submit()">
           Sign up
         </v-btn>
       </v-container>
@@ -33,15 +34,18 @@ export default {
   },
   props: ['mode'],
   methods: {
-    async login() {
-      await this.appStore.login(this.username, this.password)
-      router.push('/')
-    },
-    async signup() {
+    async submit() {
+      if (this.mode === 'login') {
+        await this.appStore.login(this.username, this.password)
+        router.push('/')
+        return
+      }
+
       if (this.password !== this.passwordConfirm) {
         alert('Passwords do not match XD')
         return
       }
+
       await this.appStore.signup(this.username, this.password)
       router.push('/')
     },
